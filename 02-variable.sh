285fb4d84ddf3d2237715292f18c5ec5dc3df367
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-18 18:02:30 +0530

    hello script

--- 02-variable.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-echo "Rajesh: : Hello Suresh, How are you?"
-echo "Suresh: : Hi rajesh, i am fine. How are you?"
-echo "rajesh: I am fine too. How is your work?"
-echo "Suresh : : not bad. i am thinking to upgrade to Devops"
