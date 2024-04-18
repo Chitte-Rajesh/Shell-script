c94563e43f673018d4db8aadc0a764e865827b33
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-18 15:02:28 +0530

    shell-script script

---
+++ variable.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Rajesh: : Hello Suresh, How are you?"
+echo "Suresh: : Hi rajesh, i am fine. How are you?"
+echo "rajesh: I am fine too. How is your work?"
+echo "uresh : : not bad. i am thinking to upgrade to Devops"
