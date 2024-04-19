a8daf0fc375794f73e290a96eb43b7d80e78151b
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-19 20:16:56 +0530

    hello script

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+Movies=("RRR" "DjTillu" "Bahubali")
+
+# size of above array is 3.
+# index are 0,1,2
+
+# list always start with 0.
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIEs[1]}"
+echo "First Movie is: ${MOVIES[@]}"
