8ea7257a43321663ce31ec6f8b377423a6c8a9ba
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-19 19:25:08 +0530

    hello script

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of variables
+PERSON1=$1
+PERSON2=$2
+
+#reffering variables
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to Devops"
