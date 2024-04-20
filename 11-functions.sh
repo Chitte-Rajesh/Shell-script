5a55d29aa27afa4dd2f9a1c692810f5029cf64f0
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-20 13:57:46 +0530

    hellow script

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%M-%H-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [$1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit=1
+    else 
+        echo "$2...SUCCESS"
+    fi
+}
+
+if [ USERID -ne 0]
+then 
+    echo "Please run this script with root access."
+    exit=1 #Manually exit if error comes.
+else
+    echo "You are super user."
+fi
