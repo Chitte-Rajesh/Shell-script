a9eefb58505376fbc81e361713baf90dcf2f7895
Rajesh-Chitte <rajeshawsdev039@gmail.com>
2024-04-20 11:44:23 +0530

    hellow script

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,17 @@
 #!bin/bash
 
-dnf install my sql -y
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+then
+    echo "please run this script with root access"
+else
+    echo "you are super user."
+fi
+
+
+
+
+
+
+dnf install my mysql -y
